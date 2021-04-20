474c0d26d24468d7a629ef88432109b87d6ecdf5
mmergal92 <mmergal92@gmail.com>
2021-04-20 13:22:18 -0400

    show and index page working and added style.css

--- Pokedex/pokedex/server.js
+++ Pokedex/pokedex/server.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({ extended: true }))
 const Pokemon = require('./models/pokemon.js');
+// console.log(Pokemon)
 const port = 3000;
+app.use('/public', express.static('public'));
 // console.log('Initial set up works')
 
 // INDEX
-app.get('/', (req, res) => {
-    res.render('index.ejs', { data: Pokemon });
+app.get('/pokemon/', (req, res) => {
+    // res.render('index.ejs', {data: Pokemon});
+    res.render('index.ejs', {
+        allPokemon: Pokemon
+    })
+    // console.log("this works")
 });
 
+//Middleware
+// app.use((req, res, next) => {
+//     console.log('I run for all routes');
+//     next();
+// });
 
 // SHOW
-app.get('/:id', (req, res) => {
-    res.render('show.ejs', { data: Pokemon[req.params.id] });
+app.get('/pokemon/:id', (req, res) => {
+    // res.render('show.ejs', {data: Pokemon[req.params.id]});
+    res.render('show.ejs', {
+        pokemons: Pokemon[req.params.id]
+    })
 });
 
 // New
